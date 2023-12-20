23361f8981376d655844484dbd938d0a307dbb69
Alina Arap <a.arap@staffcapital.nl>
2023-12-20 13:39:28 +0200

    Added page object for exchange spec

---
+++ page-objects/ExchangePage.ts
@@ -0,0 +1,42 @@
+import { expect, Locator, Page } from "@playwright/test";
+
+export class ExchangePage {
+  readonly page: Page;
+  readonly currencySelector: Locator;
+  readonly sellRate: Locator;
+  readonly amountInput: Locator;
+  readonly inDollarsCheckbox: Locator;
+  readonly calculateCostsButton: Locator;
+  readonly conversionAmount: Locator;
+  readonly purchaseButton: Locator;
+  readonly message: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.currencySelector = page.locator("#pc_currency");
+    this.sellRate = page.locator("#sp_sell_rate");
+    this.amountInput = page.locator("#pc_amount");
+    this.inDollarsCheckbox = page.locator("#pc_inDollars_true");
+    this.calculateCostsButton = page.locator("#pc_calculate_costs");
+    this.conversionAmount = page.locator("#pc_conversion_amount");
+    this.purchaseButton = page.locator("#purchase_cash");
+    this.message = page.locator("#alert_content");
+  }
+
+  async purchase() {
+    await this.currencySelector.selectOption("EUR");
+    await expect(this.sellRate).toBeVisible();
+    await this.amountInput.fill("500");
+    await this.inDollarsCheckbox.click();
+    await this.calculateCostsButton.click();
+    await expect(this.conversionAmount).toBeVisible();
+    await this.purchaseButton.click();
+  }
+
+  async asserSuccessfulPurchase() {
+    await expect(this.message).toBeVisible();
+    await expect(this.message).toContainText(
+      "Foreign currency cash was successfully purchased.",
+    );
+  }
+}
--- tests/e2e/e2e-currency-exchange.spec.ts
+++ tests/e2e/e2e-currency-exchange.spec.ts
@@ -1,14 +1,20 @@
-import { expect, test } from "@playwright/test";
+import { test } from "@playwright/test";
 import { HomePage } from "../../page-objects/HomePage";
 import { LoginPage } from "../../page-objects/LoginPage";
+import { ExchangePage } from "../../page-objects/ExchangePage";
+import { Navbar } from "../../page-objects/components/Navbar";
 
 test.describe("Payment", () => {
   let homePage: HomePage;
   let loginPage: LoginPage;
+  let exchangePage: ExchangePage;
+  let navbar: Navbar;
 
   test.beforeEach(async ({ page }) => {
     homePage = new HomePage(page);
     loginPage = new LoginPage(page);
+    exchangePage = new ExchangePage(page);
+    navbar = new Navbar(page);
 
     await homePage.visit();
     await homePage.clickOnSignInButton();
@@ -16,20 +22,10 @@ test.describe("Payment", () => {
   });
 
   test("Should make currency exchange", async ({ page }) => {
-    await page.click("#pay_bills_tab");
+    await navbar.clickOnTab("Pay Bills");
     await page.click("text=Purchase Foreign Currency");
-    await page.selectOption("#pc_currency", "EUR");
-    await page.waitForSelector("#sp_sell_rate");
-    await page.fill("#pc_amount", "500");
-    await page.click("#pc_inDollars_true");
-    await page.click("#pc_calculate_costs");
-    await page.waitForSelector("#pc_conversion_amount");
-    await page.click("#purchase_cash");
 
-    const message = page.locator("#alert_content");
-    await expect(message).toBeVisible();
-    await expect(message).toContainText(
-      "Foreign currency cash was successfully purchased.",
-    );
+    await exchangePage.purchase();
+    await exchangePage.asserSuccessfulPurchase();
   });
 });
